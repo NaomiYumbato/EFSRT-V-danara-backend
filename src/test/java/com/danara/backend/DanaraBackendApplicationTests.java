b64d311136d0cddd7efa2b11c7f5ffd81d1df6b5
Naomi <yumbatolopeznaomi@gmail.com>
2025-02-16 19:37:03 -0500

    Initial project setup

---
+++ src/test/java/com/danara/backend/DanaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danara.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanaraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
